9a7144ce65fc14a441d95f7f18cabdc98684cb6c
testinnplayin <archaurore@gmail.com>
2020-12-06 18:51:35 +0100

    Simplify ThingInterface.

--- src/interfaces/thing-interface.ts
+++ src/interfaces/thing-interface.ts
@@ -1,8 +1,4 @@
 import { Document } from "mongoose";
 import { ProtoThingDoc } from "./proto-thing-doc";
 
-export interface ThingInterface extends Document, ProtoThingDoc {
-  prop1: string;
-  prop2: string;
-  prop3: string;
-}
+export interface ThingInterface extends Document, ProtoThingDoc {}
